3c7b8fa9d42fcdf971fe0ee173fbf6c3ffbc8032
ninetyfivejae <jh33836782@gmail.com>
2017-12-02 02:17:59 +0900

    arithmetic operation initialized

--- GitCollaboratingWorkflows/app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflows/MainActivity.java
+++ GitCollaboratingWorkflows/app/src/main/java/com/example/jaehyukshin/gitcollaboratingworkflows/MainActivity.java
@@ -2,42 +2,13 @@ package com.example.jaehyukshin.gitcollaboratingworkflows;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button messiButton;
-    Button ronaldoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messiButton = (Button) findViewById(R.id.messiButton);
-        messiButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MessiShoot();
-            }
-        });
-
-        ronaldoButton = (Button) findViewById(R.id.ronaldoButton);
-        ronaldoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RonaldoPass();
-            }
-        });
-    }
-
-    public void RonaldoPass(){
-        Toast.makeText(getApplicationContext(), "Ronaldo passes the ball", Toast.LENGTH_SHORT).show();
-    }
-
-    public void MessiShoot(){
-        Toast.makeText(getApplicationContext(), "Messi shoots in the corner of the left post", Toast.LENGTH_SHORT).show();
     }
 }
